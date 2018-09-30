ca8bc36c420772c03225de3ce3062cf7edb20324
Jack Zhang <jackzhng528@gmail.com>
2018-09-30 14:45:02 -0400

    secure restful api

--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,3 @@
-modules.exports = {
+module.exports = {
 	'secret' : 'blog-app'
 };
--- routes/blog.js
+++ routes/blog.js
@@ -1,21 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var Blog = require('../models/Blog.js');
+var passport = require('passport');
+require('../config/passport')(passport);
+
+var getToken = function (headers) {
+	//Authorization: <type> <credentials>
+	if (headers && headers.authorization) {
+		var parted = headers.authorization.split(' ');
+		if (parted.length === 2) {
+			return parted[1];
+		} else {
+			return null;
+		}
+	} else {
+		return null;
+	}
+};
 
 /* GET all blogs. */
-router.get('/', function(req, res, next) {
-	Blog.find(function(err, result) {
- 		if(err) return next(err);
- 		res.json(result);
- 	});
+router.get('/', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+	var token = getToken(req.headers);
+	if(token) {
+		Blog.find(function(err, result) {
+ 			if(err) return next(err);
+ 			res.json(result);
+ 		});
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized.'});
+	}
+
 });
 
 /* Save blog. */
-router.post('/', function(req, res, next) {
-	Blog.create(req.body, function(err, post) {
-		if(err) return next(err);
-		res.json(post);
-	});
+router.post('/', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+	var token = getToken(req.headers);
+	if(token) {
+		Blog.create(req.body, function(err, post) {
+			if(err) return next(err);
+			res.json(post);
+		});
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized.'});
+	}
 });
+
+
 
 module.exports = router;
